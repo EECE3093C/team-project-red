baabf0341915a9c62f83a6378bf4957094dbf083
Preston Buterbaugh <57502915+pbuterba@users.noreply.github.com>
2023-02-21 12:51:13 -0500

    Create main.cpp
    Added basic code (includes, main function) and comments for different tasks

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //Welcome message
+
+    //Log-in to Canvas
+
+    //Fetch Canvas events and tasks
+
+    //Manually add tasks
+
+    //Manually add events
+
+    //Miscellaneous schedule constraints
+
+    //Sort tasks
+
+    //Partition into days
+
+    //Display tasks
+
+    return 0;
+}
